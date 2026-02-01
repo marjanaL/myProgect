c439178e658c75358c834444a13ccda1ebd20878
marjanaL <liylukm@gmail.com>
2026-02-01 22:27:08 +0200

    fixed

--- css/js/index-partial.js
+++ css/js/index-partial.js
@@ -1,7 +1,20 @@
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.header__menu');
+document.addEventListener('htmx:afterSwap', (e) => {
+  if (!e.target.classList.contains('header')) return;
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  menu.classList.toggle('active');
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.header__menu');
+
+  if (!burger || !menu) return;
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+
+  document.querySelectorAll('.header__menu-link').forEach(link => {
+    link.addEventListener('click', () => {
+      burger.classList.remove('active');
+      menu.classList.remove('active');
+    });
+  });
 });
