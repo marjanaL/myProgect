7a02796bac4ecec9cef0a8acc0ce58639c5ae381
marjanaL <liylukm@gmail.com>
2026-02-01 22:21:59 +0200

    h

--- css/js/index.js
+++ css/js/index.js
@@ -1,5 +1,5 @@
 function init() {
-    import('index-partial.js');
+    import('./index-partial.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
