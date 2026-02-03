a2723e8c36c6f48e4168342611084a9be6f85c88
marjanaL <liylukm@gmail.com>
2026-02-03 13:14:12 +0200

    D

--- js/index-partial.js
+++ js/index-partial.js
@@ -1,27 +1,3 @@
-/*document.body.addEventListener('click', (e) => {
-  const burger = e.target.closest('.burger');
-  if (!burger) return;
-
-  const menu = document.querySelector('.header__menu');
-  if (!menu) return;
-
-  burger.classList.toggle('active');
-  menu.classList.toggle('active');
-
-  console.log('BURGER CLICK → active:', menu.classList.contains('active'));
-});
-
-document.body.addEventListener('click', (e) => {
-  const link = e.target.closest('.header__menu-link');
-  if (!link) return;
-
-  const menu = document.querySelector('.header__menu');
-  const burger = document.querySelector('.burger');
-
-  menu?.classList.remove('active');
-  burger?.classList.remove('active');
-});*/
-
 document.body.addEventListener('click', (e) => {
   const burger = e.target.closest('.burger');
   if (!burger) return;
@@ -52,4 +28,26 @@ document.body.addEventListener('click', (e) => {
   menu?.classList.remove('active');
   burger?.classList.remove('active');
   socials?.classList.remove('active');
+});
+
+document.body.addEventListener('click', (e) => {
+  const orderBtn = e.target.closest('.pricing .button');
+  if (!orderBtn) return;
+
+  const card = orderBtn.closest('.pricing__card');
+  const plan = card.querySelector('.pricing__card-title')?.textContent;
+
+  const overlay = document.querySelector('.cart-overlay');
+  overlay.querySelector('.cart__plan').textContent = plan;
+
+  overlay.classList.add('active');
+});
+
+document.body.addEventListener('click', (e) => {
+  if (
+    e.target.closest('.cart__close') ||
+    e.target.closest('.cart__ok')
+  ) {
+    document.querySelector('.cart-overlay').classList.remove('active');
+  }
 });
